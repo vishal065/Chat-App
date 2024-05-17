1461f741ba53fd9774e50577994ae2d1b11e401e
vishal <dasvishal60@gmail.com>
2024-05-17 16:07:21 +0530

    updated .env related code

--- Backend/index.js
+++ Backend/index.js
@@ -2,6 +2,7 @@ import path from "path";
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
 
 import connectToMongoDB from "./src/DB/ConnectToMongoDb.js";
 
@@ -9,6 +10,8 @@ import { app, server } from "./src/Socket/Socket.js";
 import AuthRouter from "./src/Routes/Auth.router.js";
 import MessageRouter from "./src/Routes/Message.router.js";
 import UsersRouter from "./src/Routes/Users.router.js";
+
+dotenv.config();
 
 app.use(cors({ origin: "*" }));
 app.use(express.json());
